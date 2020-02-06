97e4ecd53ded81085962068a79036ee05ded9003
Pankaj <pgrover2079@gmail.com>
2020-02-05 16:49:54 -0800

    Both components are Dockerized

--- server/index.js
+++ server/index.js
@@ -15,14 +15,23 @@ const clientPath = '../client';
 
 app.use(express.static(path.join(__dirname, clientPath)));
 
-app.get('/carousel', (req, res) => {
-  axios.get('http://localhost:2500/bundle.js')
+app.get('/carousel', (req, res) => {                  // changed localhost to service path
+  axios.get('http://carousel:2500/bundle.js')        // This is service path as specified in yml file
   .then(response => res.send(response.data))
+  .catch((error) => (console.log('Error getting Carousel component')))
 });
 
+
+// AWS S3
+// app.get('/carousel', (req, res) => {
+//   axios.get('https://hrcarousel.s3-us-west-1.amazonaws.com/carousel-static/bundle.js')
+//   .then(response => res.send(response.data))
+// });
+
 app.get('/checkout', (req, res) => {
-  axios.get('http://localhost:2000/bundle.js')
+  axios.get('http://checkout:2000/bundle.js')      // This is service path as specified in yml file
   .then(response => res.send(response.data))
+  .catch((error) => (console.log('Error getting Checkout component')))
 });
 
 app.listen(PORT, () => console.log(`Server listening to PORT: ${PORT}`))
